2cfd139f431cea0304bda4726eb779ac11e96a35
Your Name <you@example.com>
2022-02-04 16:30:47 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -10,10 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
-      home: HomePage()
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context)  => new LoginPage(),
+        "/home": (context ) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
